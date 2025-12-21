461d7cd64aab467710d5850149f9f4f258f8fab2
LFGA <braiselala@gmail.com>
2025-12-21 16:07:49 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import {useEffect, useState} from "react";
+import SearchBar from "./components/SearchBar";
+import CurrentWeather from "./components/CurrentWeather";
+import DailyForecast from "./components/DailyForecast";
+import {getWeatherData} from "./services/weatherApi";
+function App(){
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("Kigali");
+  const [error, setError] = useState(" ");
+
+  useEffect(()=>{
+    fetchWeather(city);
+  }, []);
+
+  async function fetchWeather(cityName){
+    try{
+      const data = await getWeatherData(cityName);
+      setWeather(data);
+      setError(" ");
+    }catch{
+      setError("City not found");
+    }
+  }
+
+  return(
+    <div style = {{padding: 30, fontFamily: "Arial"}}>
+      <SearchBar onSearch = {fetchWeather} />
+
+      {error && <p style = {{color: "red"}}>{error}</p>}
+
+      {weather && (
+        <>
+        <CurrentWeather data = {weather.current} city={weather.city} />
+        <DailyForecast data = {weather.daily} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default App;
+
